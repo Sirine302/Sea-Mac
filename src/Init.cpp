65bca5ff1e61dce992dee4596bd8882c8a75a9be
Sirine302 <64043568+Sirine302@users.noreply.github.com>
2021-05-11 19:33:06 +0200

    Add files via upload

---
+++ src/Init.cpp
@@ -0,0 +1,209 @@
+#include "main.h"
+
+// plein ecran
+void ChangeToFullScreen()
+{
+	DEVMODE dmSettings;									
+
+	memset(&dmSettings,0,sizeof(dmSettings));		
+
+	if(!EnumDisplaySettings(NULL,ENUM_CURRENT_SETTINGS,&dmSettings))
+	{
+		MessageBox(NULL, "Could Not Enum Display Settings", "Error", MB_OK);
+		return;
+	}
+
+	dmSettings.dmPelsWidth	= SCREEN_WIDTH;				// largeur de l'ecran
+	dmSettings.dmPelsHeight	= SCREEN_HEIGHT;			// hauteur de l'ecrn
+	dmSettings.dmFields     = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+	// fct qui met en plein ecran
+	int result = ChangeDisplaySettings(&dmSettings,CDS_FULLSCREEN);	
+
+
+	if(result != DISP_CHANGE_SUCCESSFUL)
+	{
+		// msg erreur + quitte le programme
+		MessageBox(NULL, "Display Mode Not Compatible", "Error", MB_OK);
+		PostQuitMessage(0);
+	}
+}
+
+
+// creation de la fenetre
+HWND CreateMyWindow(LPSTR strWindowName, int width, int height, DWORD dwStyle, bool bFullScreen, HINSTANCE hInstance)
+{
+	HWND hWnd;
+	WNDCLASS wndclass;
+	
+	memset(&wndclass, 0, sizeof(WNDCLASS));				// Init taille de la classe
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;			
+	wndclass.lpfnWndProc = WinProc;						
+	wndclass.hInstance = hInstance;						
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);	
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);		// fleche pour la souris
+	wndclass.hbrBackground = (HBRUSH) (COLOR_WINDOW+1);	// fenetre de couleur blanhe
+	wndclass.lpszClassName = "GameTutorials";			
+
+	RegisterClass(&wndclass);							
+	
+	if(bFullScreen && !dwStyle) 						// verifie si on veut un plein ecran
+	{													// mode plein ecran
+		dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+		ChangeToFullScreen();							// passage en plein ecran
+		ShowCursor(FALSE);								// on cache le cureur
+	}
+	else if(!dwStyle)									
+		dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+	
+	g_hInstance = hInstance;						
+
+	RECT rWindow;
+	rWindow.left	= 0;								
+	rWindow.right	= width;							
+	rWindow.top	    = 0;								
+	rWindow.bottom	= height;							
+
+	AdjustWindowRect( &rWindow, dwStyle, false);		
+
+														// Creation de la fenetre
+	hWnd = CreateWindow("VisuTerrImac", strWindowName, dwStyle, 0, 0,
+						rWindow.right  - rWindow.left, rWindow.bottom - rWindow.top, 
+						NULL, NULL, hInstance, NULL);
+
+	if(!hWnd) return NULL;								
+
+	ShowWindow(hWnd, SW_SHOWNORMAL);					// affichae fenetre
+	UpdateWindow(hWnd);									
+
+	SetFocus(hWnd);										// clavier focus par rapport a la fenetre	
+
+	return hWnd;
+}
+
+// format pixel pour opengl
+bool bSetupPixelFormat(HDC hdc) 
+{ 
+    PIXELFORMATDESCRIPTOR pfd; 
+    int pixelformat; 
+ 
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);			
+    pfd.nVersion = 1;									
+														
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER; 
+    pfd.dwLayerMask = PFD_MAIN_PLANE;					
+    pfd.iPixelType = PFD_TYPE_RGBA;						
+    pfd.cColorBits = SCREEN_DEPTH;						
+    pfd.cDepthBits = SCREEN_DEPTH;						
+    pfd.cAccumBits = 0;									
+    pfd.cStencilBits = 0;								
+ 
+	
+    if ( (pixelformat = ChoosePixelFormat(hdc, &pfd)) == FALSE ) 
+    { 
+        MessageBox(NULL, "ChoosePixelFormat failed", "Error", MB_OK); 
+        return FALSE; 
+    } 
+ 
+    if (SetPixelFormat(hdc, pixelformat, &pfd) == FALSE) 
+    { 
+        MessageBox(NULL, "SetPixelFormat failed", "Error", MB_OK); 
+        return FALSE; 
+    } 
+ 
+    return TRUE;										
+}
+
+
+void SizeOpenGLScreen(int width, int height)			// Init gl window
+{
+	if (height==0)										
+	{
+		height=1;										
+	}
+
+	glViewport(0,0,width,height);						
+														
+														
+														
+
+	glMatrixMode(GL_PROJECTION);						
+	glLoadIdentity();									
+
+														
+														
+														 
+														 
+
+
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height, 4 , 4000.0f);
+
+
+
+	glMatrixMode(GL_MODELVIEW);							
+	glLoadIdentity();									
+}
+
+
+
+void InitializeOpenGL(int width, int height) 
+{  
+    g_hDC = GetDC(g_hWnd);							
+														
+    if (!bSetupPixelFormat(g_hDC))						
+        PostQuitMessage (0);							
+
+    g_hRC = wglCreateContext(g_hDC);					
+    wglMakeCurrent(g_hDC, g_hRC);						
+
+	glEnable(GL_DEPTH_TEST);							
+
+	SizeOpenGLScreen(width, height);					
+}
+
+
+
+void DeInit()
+{
+	if (g_hRC)											
+	{
+		wglMakeCurrent(NULL, NULL);						
+		wglDeleteContext(g_hRC);							
+	}
+	
+	if (g_hDC) 
+		ReleaseDC(g_hWnd, g_hDC);						
+		
+	if(g_bFullScreen)									
+	{
+		ChangeDisplaySettings(NULL,0);					
+		ShowCursor(TRUE);								
+	}
+
+	UnregisterClass("IMAC", g_hInstance);		
+
+	PostQuitMessage (0);								
+}
+
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hprev, PSTR cmdline, int ishow)
+{	
+	HWND hWnd;
+
+
+	if(MessageBox(NULL, "Passer en plein ecran", "Options", MB_YESNO | MB_ICONQUESTION) == IDNO)
+		g_bFullScreen = false;
+	
+	
+	hWnd = CreateMyWindow("IMAC", SCREEN_WIDTH, SCREEN_HEIGHT, 0, g_bFullScreen, hInstance);
+
+	
+	if(hWnd == NULL) return TRUE;
+
+
+	Init(hWnd);													
+
+	return MainLoop();						
+}
